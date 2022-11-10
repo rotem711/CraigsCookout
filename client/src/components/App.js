5a153f9e8c3fd1facdd97201c9316db389340ea5
rotem711 <rotemskandrani@gmail.com>
2022-11-10 16:45:26 -0600

    Adding V6 style React routing components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import Login from "../pages/Login";
-import RecipeList from "../pages/RecipeList";
-import NewRecipe from "../pages/NewRecipe";
 
 function App() {
   const [user, setUser] = useState(null);
