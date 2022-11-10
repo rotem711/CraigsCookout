a5c077542fa798e05a74a96805ec68e0d34c007a
rotem711 <rotemskandrani@gmail.com>
2022-11-09 21:13:44 -0600

    Adding revisions to SessionsController to allow for login and logout via create and destroy methods

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :foods, only: [:index, :create]
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
