2ed4148edacecbdfcf6f612892f4bb1b7ca69770
rotem711 <rotemskandrani@gmail.com>
2022-11-09 21:06:35 -0600

    Adding User controller with corresponding revisions to overarching Application Controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
+  resources :cookouts, only: [:index, :create]
+  resources :foods, only: [:index, :create]
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
