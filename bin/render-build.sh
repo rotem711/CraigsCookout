320c2d36d7362dac7f15f111bb5af33d80f9dd62
rotem711 <rotemskandrani@gmail.com>
2023-01-14 14:02:13 -0600

    Overhauled project with render template again to figure out why it's not deploying correctly

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,12 +2,14 @@
 # exit on error
 set -o errexit
 
-# builds the front end code
-rm -rf public
+# NOTE:
+# Adding build commands for frontend for Render.com
+rm -rf pulic
 npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
-# builds the back end code
 bundle install
-bundle exec rake db:migrate
-bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only
+bundle exec rake db:migrate 
+# NOTE:
+# Commenting out seed data since we already seeded the database locally:
+# bundle exec rake db:seed
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,16 +5,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    # NOTE: Here is what was previously present
-    # origins 'example.com'
-    # NOTE: I had to change the 'origins' parameter value to '*' so it was accepting of any value including a 'localhost'
-    # web address for testing purposes:
-    origins '*'
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-end
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'example.com'
+#
+#     resource '*',
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#   end
+# end
