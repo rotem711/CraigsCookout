a3e9847510f3a07a8ef6e5b94af79128a13db419
rotem711 <rotemskandrani@gmail.com>
2023-01-15 16:25:03 -0600

    Overhauling project one last time to see if Flatiron's original template even works for Render deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,14 +2,11 @@
 # exit on error
 set -o errexit
 
-# NOTE:
-# Adding build commands for frontend for Render.com
+# Add build commands for front end
 rm -rf public
 npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
 bundle install
 bundle exec rake db:migrate 
-# NOTE:
-# Commenting out seed data since we already seeded the database locally:
-# bundle exec rake db:seed
+bundle exec rake db:seed
